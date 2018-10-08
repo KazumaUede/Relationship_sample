a2930eb67328f0e33c23148bf4b2f9e8018e379e
KazumaUede <katsuodashi794@gmail.com>
2018-10-08 15:18:46 +0900

    Add DM

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages
+  has_many :group_users
+  has_many :groups, through: :group_users
 
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
@@ -32,6 +34,10 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
 
+  # def matching?
+  #   followers.include?(other_user)
+  # end
+
 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships,  class_name:  "Relationship",
@@ -43,6 +49,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  # 相互フォローを判定する
   def matchers
     User.where(id: passive_relationships.select(:follower_id))
      .where(id: active_relationships.select(:followed_id))
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :messages, only: [:index, :show, :create]
+  resources :groups, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   resources :relationships, only: [:create, :destroy]
   resources :users do
     member do
